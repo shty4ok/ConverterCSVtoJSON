f5eed2aed7182fd7b4fa9679ebbec4db77429280
unknown <you@example.com>
2020-05-16 21:47:49 +0300

    not final

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Papa = require('papaparse');
 const file = 'translate.csv';
 const content = fs.readFileSync(file, 'utf8');
 let rows;
+let o;
 
 //=================================================
 //CSV TO JSON
@@ -11,35 +12,22 @@ Papa.parse(content, {
     header: false,
     delimiter: ";",
     complete: function (results) {
-        // console.log("Finished:", results.data);
         rows = results.data;
-        // console.log(rows);
         createObj(rows);
     }
 });
+function createObj(arr) {
+     arr.shift();
 
-function createObj(chain) {
-    let elemArray = 0;
-    let pushElem = [];
-    let n = 0;
-    let pushObj = {};
-    let finalObj = {};
-    let sashPasha = {};
-    for (let i = 1; i < chain.length; i++) {
-        elemArray = chain[i][0];
-        if (elemArray) {
-            pushElem = elemArray.split('.');
-        }
+    const data =  arr.map((item, k, arr)=> {
+        return {'path': item[0], 'translate': item[1]};
+    });
+       data.forEach((item,k)=> {
+            o = item.path;
+        });
+    for (let i = 0; i < data.length; i++) {
+        // o.split('.');
 
-        let max = pushElem.length-1;
-        finalObj[pushElem[max]] = chain[i][1]; //login: 'Логин'
-        // for (let j = max; j >= 0; j--) {
-            pushObj[pushElem[max-1]] = finalObj;
-        // }
-        sashPasha = pushObj;
-        finalObj = {};
-        pushObj = {};
-        // console.log(sashPasha);
-    console.log(sashPasha);
     }
+    console.log(o);
 }
