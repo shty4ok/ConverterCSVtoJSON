8a2cec415c2796fdf8b39da3606160bb65c8c46a
unknown <you@example.com>
2020-09-19 13:44:15 +0300

    work version

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const Papa = require('papaparse');
 const file = 'translate.csv';
+const fileExport = 'translate.json';
 const content = fs.readFileSync(file, 'utf8');
 let rows;
-let o;
+let g = {};
 
 //=================================================
 //CSV TO JSON
@@ -16,18 +17,31 @@ Papa.parse(content, {
         createObj(rows);
     }
 });
-function createObj(arr) {
-     arr.shift();
 
-    const data =  arr.map((item, k, arr)=> {
+function createObj(arr) {
+    arr.shift();
+
+    const data = arr.map((item, k, arr) => {
         return {'path': item[0], 'translate': item[1]};
     });
-       data.forEach((item,k)=> {
-            o = item.path;
-        });
-    for (let i = 0; i < data.length; i++) {
-        // o.split('.');
+    data.forEach((item) => {
+        let helper = g;
+        // item = { path, translate }
+        const path = item.path.split('.');
 
-    }
-    console.log(o);
+        for (let i = 0; i < path.length - 1; i++) {
+            if (!helper[path[i]]) {
+                helper[path[i]] = {};
+            }
+
+            helper = helper[path[i]];
+        }
+
+        helper[path[path.length - 1]] = item.translate;
+    });
+    console.dir(g);
+    fs.writeFileSync('translate.json', JSON.stringify(g),(err)=> {
+        if (err) throw err;
+        console.log('CSV CREATE');
+    })
 }
